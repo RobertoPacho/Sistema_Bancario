ba63c58abba5ce20858ee2308d71817404e9ea12
RobertoPacho <robertsonam1993@gmail.com>
2021-01-14 19:51:39 -0500

    agregacion metodos poliza

--- src/main/java/ec/ups/edu/Banca_Movil/Beans/TasaInteresBEAN.java
+++ src/main/java/ec/ups/edu/Banca_Movil/Beans/TasaInteresBEAN.java
@@ -22,30 +22,129 @@ import ec.ups.edu.Banca_Movil.on.TasaInteresON;
 @Named
 @RequestScoped
 public class TasaInteresBEAN {
-	
+
 	@Inject
 	TasaInteresON tasaInteresON;
-	
-	@Inject	
+
+	@Inject
 	TasaInteres tasaInteres;
-	
-	//Modificar check box
-    private boolean tasa1;
+
+	// Modificar check box
+	private boolean tasa1;
 	private boolean tasa2;
 	private boolean tasa3;
 	private boolean tasa4;
 	private boolean tasa5;
 	private boolean tasa6;
 	//
-	private String etasa1;
-	
-	
-	public String getEtasa1() {
+	private double etasa1;
+	private double etasa2;
+	private double etasa3;
+	private double etasa4;
+	private double etasa5;
+	private double etasa6;
+	//
+	private String etiempo1 = "30-59";
+	private String etiempo2 = "60-89";
+	private String etiempo3 = "90-179";
+	private String etiempo4 = "180-269";
+	private String etiempo5 = "270-359";
+	private String etiempo6 = "Mas de 360";
+
+	public String getEtiempo1() {
+		return etiempo1;
+	}
+
+	public void setEtiempo1(String etiempo1) {
+		this.etiempo1 = etiempo1;
+	}
+
+	public String getEtiempo2() {
+		return etiempo2;
+	}
+
+	public void setEtiempo2(String etiempo2) {
+		this.etiempo2 = etiempo2;
+	}
+
+	public String getEtiempo3() {
+		return etiempo3;
+	}
+
+	public void setEtiempo3(String etiempo3) {
+		this.etiempo3 = etiempo3;
+	}
+
+	public String getEtiempo4() {
+		return etiempo4;
+	}
+
+	public void setEtiempo4(String etiempo4) {
+		this.etiempo4 = etiempo4;
+	}
+
+	public String getEtiempo5() {
+		return etiempo5;
+	}
+
+	public void setEtiempo5(String etiempo5) {
+		this.etiempo5 = etiempo5;
+	}
+
+	public String getEtiempo6() {
+		return etiempo6;
+	}
+
+	public void setEtiempo6(String etiempo6) {
+		this.etiempo6 = etiempo6;
+	}
+
+	public double getEtasa1() {
 		return etasa1;
 	}
 
-	public void setEtasa1(String etasa1) {
+	public void setEtasa1(double etasa1) {
 		this.etasa1 = etasa1;
+	}
+
+	public double getEtasa2() {
+		return etasa2;
+	}
+
+	public void setEtasa2(double etasa2) {
+		this.etasa2 = etasa2;
+	}
+
+	public double getEtasa3() {
+		return etasa3;
+	}
+
+	public void setEtasa3(double etasa3) {
+		this.etasa3 = etasa3;
+	}
+
+	public double getEtasa4() {
+		return etasa4;
+	}
+
+	public void setEtasa4(double etasa4) {
+		this.etasa4 = etasa4;
+	}
+
+	public double getEtasa5() {
+		return etasa5;
+	}
+
+	public void setEtasa5(double etasa5) {
+		this.etasa5 = etasa5;
+	}
+
+	public double getEtasa6() {
+		return etasa6;
+	}
+
+	public void setEtasa6(double etasa6) {
+		this.etasa6 = etasa6;
 	}
 
 	public boolean isTasa1() {
@@ -95,20 +194,58 @@ public class TasaInteresBEAN {
 	public void setTasa6(boolean tasa6) {
 		this.tasa6 = tasa6;
 	}
+
 	public void addMessage() {
-		if(tasa1) {
+		if (tasa1) {
 			String summary = tasa1 ? "Checked" : "Unchecked";
 			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(summary));
-			System.out.println(""+etasa1);
+			tasaInteresON.updateTasa(1, 5.55);
+			System.out.println("Tasa nueva = " + etasa1);
 		}
-    }
-	
-	public List<TasaInteres> listaInteresesBean() throws Exception{
-    	return tasaInteresON.listaTasaInteres();
-    }
+	}
 
-	
-	
-	
+	public List<TasaInteres> listaInteresesBean() throws Exception {
+		return tasaInteresON.listaTasaInteres();
+	}
+
+	public void prueba() throws Exception {
+		if (tasa1) {
+			tasaInteres.setId(1);
+			tasaInteres.setTasa(etasa1);
+			tasaInteres.setTiempo(etiempo1);
+			tasaInteresON.actualizar(tasaInteres);
+		}
+		if (tasa2) {
+			tasaInteres.setId(2);
+			tasaInteres.setTasa(etasa2);
+			tasaInteres.setTiempo(etiempo2);
+			tasaInteresON.actualizar(tasaInteres);
+		}
+		if (tasa3) {
+			tasaInteres.setId(3);
+			tasaInteres.setTasa(etasa3);
+			tasaInteres.setTiempo(etiempo3);
+			tasaInteresON.actualizar(tasaInteres);
+
+		}
+		if (tasa4) {
+			tasaInteres.setId(4);
+			tasaInteres.setTasa(etasa4);
+			tasaInteres.setTiempo(etiempo4);
+			tasaInteresON.actualizar(tasaInteres);
+		}
+		if (tasa5) {
+			tasaInteres.setId(5);
+			tasaInteres.setTasa(etasa5);
+			tasaInteres.setTiempo(etiempo5);
+			tasaInteresON.actualizar(tasaInteres);
+		}
+		if (tasa6) {
+			tasaInteres.setId(6);
+			tasaInteres.setTasa(etasa6);
+			tasaInteres.setTiempo(etiempo6);
+			tasaInteresON.actualizar(tasaInteres);
+		}
+	}
 
 }
